54924e6f728e32b49f00c369348f895498d3a544
Justin Gomez <programjustin@gmail.com>
2017-04-28 12:20:57 -0700

    add a full test of all five survey questions to the user and change the height of the header and margin-top of the h1 element inside the header

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,42 @@ $(document).ready(function() {
      var moreTracks  = $("input:radio[name=moreTracks]:checked").val();
 
      if(ruby === "option1" && design === "option2" && php === "option3") {
-       alert("you chose ruby, design and php");
-     } else if (ruby === "option1" && design === "option2") {
-       alert("ruby and design");
-     }
 
+       alert("you chose ruby, design and php");
+
+     } else if (ruby === "option1" && design === "option2") {
+
+       alert("ruby and design");
+
+     } else if (ruby === "option1" && php === "option3") {
+
+       alert("ruby and php");
+
+     } else if (design === "option2" && php === "option3") {
+
+       alert("deisgn and php");
+
+     } else if (ruby === "option1") {
+
+       alert("ruby");
+
+     } else if (design === "option2") {
+
+       alert("design");
+
+     } else if (php === "option3") {
+
+       alert("php");
+
+     } else if (cHash === "option5") {
+
+        alert("c#")
+
+     } else if (moreTracks) {
+
+       alert("more Tracks");
+
+     }
 
   });
 });
